0d510ce406d9e07f1e53c9741afe8255febac813
Serverguyken <serverguyken@gmail.com>
2022-04-07 06:07:28 -0400

    fixed loading width

--- components/FeedProfile.tsx
+++ components/FeedProfile.tsx
@@ -15,6 +15,9 @@ import PullToRefresh from 'react-simple-pull-to-refresh';
 import { ArrowUpIcon, ArrowDownIcon } from '@heroicons/react/outline';
 import { Spinner } from '../utils/loader'
 import ProfileCardHover from './ProfileCardHover'
+import Skeleton from './Skeleton'
+
+
 function countSet(num: number) {
     let value = `${num}`
     let unit = ''
@@ -194,9 +197,14 @@ const FeedProfile = () => {
             </div>
             {
                 !rendered ?
-                    <div className='screen-sm:pt-24 flex absolute top-60 left-1/2  justify-center'>
-                        <Spinner width={24} color='var(--color-primary)' />
-                        
+                    <div className='screen-sm:pt-24'>
+                        <div className='opacity-0'>
+                            <Skeleton type="feed" />
+                        </div>
+                        <div className='flex absolute top-60 left-1/2  justify-center'>
+
+                            <Spinner width={24} color='var(--color-primary)' />
+                        </div>
                     </div>
                     :
                     <PullToRefresh
@@ -378,132 +386,7 @@ const FeedProfile = () => {
                                         )
                                     })
                                     :
-                                    <div className="skeleton_feed mt-5 p-2 w-full max-w-4xl">
-                                        <div className="skeleton_feed_contents flex space-x-2">
-                                            <div className="skeleton_feed_contents_header">
-                                                <div className="skeleton_feed_header_image bg-gray-300 w-8 h-8 rounded-full animate-pulse">
-                                                </div>
-                                            </div>
-                                            <div className="skeleton_feed_post_contents mt-1">
-                                                <div className="skeleton_contents_username">
-                                                    <div className="skeleton_feed_fullname bg-gray-300 w-40 h-3 rounded-sm animate-pulse"></div>
-                                                    <div className="skeleton_feed_username bg-gray-300 mt-1 w-24 h-3 rounded-sm animate-pulse"></div>
-                                                    <div className="skeleton_feed_post_contents bg-gray-300 mt-3 w-full h-24 rounded-sm animate-pulse"><p className='opacity-0'>Content will be shown immidiately after freshing. Please wait...</p></div>
-                                                    <div className="flex justify-between mt-5">
-                                                        <div className="flex space-x-2 items-center">
-                                                            <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
-                                                            <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
-                                                        </div>
-                                                        <div className="flex space-x-2 items-center">
-                                                            <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
-                                                            <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
-                                                        </div>
-                                                        <div className="flex space-x-2 items-center">
-                                                            <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
-                                                            <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
-                                                        </div>
-                                                        <div className="flex space-x-2 items-center">
-                                                            <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
-                                                            <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="skeleton_feed_contents mt-10 flex space-x-2">
-                                            <div className="skeleton_feed_contents_header">
-                                                <div className="skeleton_feed_header_image bg-gray-300 w-8 h-8 rounded-full animate-pulse">
-                                                </div>
-                                            </div>
-                                            <div className="skeleton_feed_post_contents mt-1">
-                                                <div className="skeleton_contents_username">
-                                                    <div className="skeleton_feed_fullname bg-gray-300 w-40 h-3 rounded-sm animate-pulse"></div>
-                                                    <div className="skeleton_feed_username bg-gray-300 mt-1 w-24 h-3 rounded-sm animate-pulse"></div>
-                                                    <div className="skeleton_feed_post_contents bg-gray-300 mt-3 w-full h-24 rounded-sm animate-pulse"><p className='opacity-0'>Content will be shown immidiately after freshing. Please wait...</p></div>
-                                                    <div className="flex justify-between mt-5">
-                                                        <div className="flex space-x-2 items-center">
-                                                            <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
-                                                            <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
-                                                        </div>
-                                                        <div className="flex space-x-2 items-center">
-                                                            <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
-                                                            <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
-                                                        </div>
-                                                        <div className="flex space-x-2 items-center">
-                                                            <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
-                                                            <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
-                                                        </div>
-                                                        <div className="flex space-x-2 items-center">
-                                                            <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
-                                                            <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="skeleton_feed_contents mt-10 flex space-x-2">
-                                            <div className="skeleton_feed_contents_header">
-                                                <div className="skeleton_feed_header_image bg-gray-300 w-8 h-8 rounded-full animate-pulse">
-                                                </div>
-                                            </div>
-                                            <div className="skeleton_feed_post_contents mt-1">
-                                                <div className="skeleton_contents_username">
-                                                    <div className="skeleton_feed_fullname bg-gray-300 w-40 h-3 rounded-sm animate-pulse"></div>
-                                                    <div className="skeleton_feed_username bg-gray-300 mt-1 w-24 h-3 rounded-sm animate-pulse"></div>
-                                                    <div className="skeleton_feed_post_contents bg-gray-300 mt-3 w-full h-24 rounded-sm animate-pulse"><p className='opacity-0'>Content will be shown immidiately after freshing. Please wait...</p></div>
-                                                    <div className="flex justify-between mt-5">
-                                                        <div className="flex space-x-2 items-center">
-                                                            <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
-                                                            <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
-                                                        </div>
-                                                        <div className="flex space-x-2 items-center">
-                                                            <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
-                                                            <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
-                                                        </div>
-                                                        <div className="flex space-x-2 items-center">
-                                                            <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
-                                                            <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
-                                                        </div>
-                                                        <div className="flex space-x-2 items-center">
-                                                            <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
-                                                            <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="skeleton_feed_contents mt-10 flex space-x-2">
-                                            <div className="skeleton_feed_contents_header">
-                                                <div className="skeleton_feed_header_image bg-gray-300 w-8 h-8 rounded-full animate-pulse">
-                                                </div>
-                                            </div>
-                                            <div className="skeleton_feed_post_contents mt-1">
-                                                <div className="skeleton_contents_username">
-                                                    <div className="skeleton_feed_fullname bg-gray-300 w-40 h-3 rounded-sm animate-pulse"></div>
-                                                    <div className="skeleton_feed_username bg-gray-300 mt-1 w-24 h-3 rounded-sm animate-pulse"></div>
-                                                    <div className="skeleton_feed_post_contents bg-gray-300 mt-3 w-full h-24 rounded-sm animate-pulse"><p className='opacity-0'>Content will be shown immidiately after freshing. Please wait...</p></div>
-                                                    <div className="flex justify-between mt-5">
-                                                        <div className="flex space-x-2 items-center">
-                                                            <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
-                                                            <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
-                                                        </div>
-                                                        <div className="flex space-x-2 items-center">
-                                                            <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
-                                                            <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
-                                                        </div>
-                                                        <div className="flex space-x-2 items-center">
-                                                            <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
-                                                            <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
-                                                        </div>
-                                                        <div className="flex space-x-2 items-center">
-                                                            <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
-                                                            <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    <Skeleton type="feed" />
                             }
                         </div>
                     </PullToRefresh>
---
+++ components/Skeleton.tsx
@@ -0,0 +1,145 @@
+
+const FeedSkeleton = () => {
+    return (
+        <div className="skeleton_feed mt-5 p-2">
+            <div className="skeleton_feed_contents flex space-x-2">
+                <div className="skeleton_feed_contents_header">
+                    <div className="skeleton_feed_header_image bg-gray-300 w-8 h-8 rounded-full animate-pulse">
+                    </div>
+                </div>
+                <div className="skeleton_feed_post_contents mt-1">
+                    <div className="skeleton_contents_username">
+                        <div className="skeleton_feed_fullname bg-gray-300 w-40 h-3 rounded-sm animate-pulse"></div>
+                        <div className="skeleton_feed_username bg-gray-300 mt-1 w-24 h-3 rounded-sm animate-pulse"></div>
+                        <div className="skeleton_feed_post_contents bg-gray-300 mt-3 w-auto h-24 rounded-sm animate-pulse"><p className='opacity-0'>Content will be shown immidiately after freshing. Please wait...Content will be shown immidiately after freshing. Please wait...</p></div>
+                        <div className="flex justify-between mt-5">
+                            <div className="flex space-x-2 items-center">
+                                <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
+                                <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
+                            </div>
+                            <div className="flex space-x-2 items-center">
+                                <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
+                                <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
+                            </div>
+                            <div className="flex space-x-2 items-center">
+                                <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
+                                <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
+                            </div>
+                            <div className="flex space-x-2 items-center">
+                                <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
+                                <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="skeleton_feed_contents flex space-x-2">
+                <div className="skeleton_feed_contents_header">
+                    <div className="skeleton_feed_header_image bg-gray-300 w-8 h-8 rounded-full animate-pulse">
+                    </div>
+                </div>
+                <div className="skeleton_feed_post_contents mt-1">
+                    <div className="skeleton_contents_username">
+                        <div className="skeleton_feed_fullname bg-gray-300 w-40 h-3 rounded-sm animate-pulse"></div>
+                        <div className="skeleton_feed_username bg-gray-300 mt-1 w-24 h-3 rounded-sm animate-pulse"></div>
+                        <div className="skeleton_feed_post_contents bg-gray-300 mt-3 w-auto h-24 rounded-sm animate-pulse"><p className='opacity-0'>Content will be shown immidiately after freshing. Please wait...Content will be shown immidiately after freshing. Please wait...</p></div>
+                        <div className="flex justify-between mt-5">
+                            <div className="flex space-x-2 items-center">
+                                <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
+                                <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
+                            </div>
+                            <div className="flex space-x-2 items-center">
+                                <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
+                                <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
+                            </div>
+                            <div className="flex space-x-2 items-center">
+                                <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
+                                <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
+                            </div>
+                            <div className="flex space-x-2 items-center">
+                                <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
+                                <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="skeleton_feed_contents flex space-x-2">
+                <div className="skeleton_feed_contents_header">
+                    <div className="skeleton_feed_header_image bg-gray-300 w-8 h-8 rounded-full animate-pulse">
+                    </div>
+                </div>
+                <div className="skeleton_feed_post_contents mt-1">
+                    <div className="skeleton_contents_username">
+                        <div className="skeleton_feed_fullname bg-gray-300 w-40 h-3 rounded-sm animate-pulse"></div>
+                        <div className="skeleton_feed_username bg-gray-300 mt-1 w-24 h-3 rounded-sm animate-pulse"></div>
+                        <div className="skeleton_feed_post_contents bg-gray-300 mt-3 w-auto h-24 rounded-sm animate-pulse"><p className='opacity-0'>Content will be shown immidiately after freshing. Please wait...Content will be shown immidiately after freshing. Please wait...</p></div>
+                        <div className="flex justify-between mt-5">
+                            <div className="flex space-x-2 items-center">
+                                <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
+                                <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
+                            </div>
+                            <div className="flex space-x-2 items-center">
+                                <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
+                                <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
+                            </div>
+                            <div className="flex space-x-2 items-center">
+                                <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
+                                <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
+                            </div>
+                            <div className="flex space-x-2 items-center">
+                                <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
+                                <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="skeleton_feed_contents flex space-x-2">
+                <div className="skeleton_feed_contents_header">
+                    <div className="skeleton_feed_header_image bg-gray-300 w-8 h-8 rounded-full animate-pulse">
+                    </div>
+                </div>
+                <div className="skeleton_feed_post_contents mt-1">
+                    <div className="skeleton_contents_username">
+                        <div className="skeleton_feed_fullname bg-gray-300 w-40 h-3 rounded-sm animate-pulse"></div>
+                        <div className="skeleton_feed_username bg-gray-300 mt-1 w-24 h-3 rounded-sm animate-pulse"></div>
+                        <div className="skeleton_feed_post_contents bg-gray-300 mt-3 w-auto h-24 rounded-sm animate-pulse"><p className='opacity-0'>Content will be shown immidiately after freshing. Please wait...Content will be shown immidiately after freshing. Please wait...</p></div>
+                        <div className="flex justify-between mt-5">
+                            <div className="flex space-x-2 items-center">
+                                <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
+                                <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
+                            </div>
+                            <div className="flex space-x-2 items-center">
+                                <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
+                                <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
+                            </div>
+                            <div className="flex space-x-2 items-center">
+                                <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
+                                <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
+                            </div>
+                            <div className="flex space-x-2 items-center">
+                                <div className="skeleton_feed_fullname bg-gray-300 w-5 h-5 rounded-full animate-pulse"></div>
+                                <div className="skeleton_feed_username bg-gray-300 mt-1 w-6 h-3 rounded-sm animate-pulse"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+const Skeleton = ({ type }: {
+    type: string
+}) => { 
+    switch (type) {
+        case 'feed':
+            return <FeedSkeleton />
+        default:
+            return <div>No skeleton</div>
+    }
+};
+
+
+
+export default Skeleton;
